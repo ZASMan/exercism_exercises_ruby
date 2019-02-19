ee10a91ff4d320e6c106d696c76f4208e1a3e5dc
zasman <zanesandin@gmail.com>
2019-02-19 13:46:16 -0500

    Updates series

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -9,20 +9,6 @@ class Series
     if substring_length > @numbers_array.length
       raise ArgumentError.new("Substring length cannot exceed number array length.")
     end
-  	result = []
-    starting_index = 0
-    substring_length_doesnt_exceed_string = true
-    while substring_length_doesnt_exceed_string == true
-      substring_array = @numbers_array[starting_index, substring_length]
-      substring = substring_array.join
-      result << substring
-      starting_index = starting_index + 1
-      if starting_index > @numbers_array.length
-        substring_length_doesnt_exceed_string = false
-      end
-    end
-    # Remove result elements which are less than substring legnth
-    result.reject! { |element| element.length < substring_length }
-    result.compact.flatten.map! { |element| element.to_s }
+    @numbers_array.each_cons(substring_length).to_a.map { |array| array.join }
   end
 end
